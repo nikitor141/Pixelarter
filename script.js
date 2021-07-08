c83efebceffc9dd8610fdb9a9a1b4a6b67861502
nikitor141 <68957821+nikitor141@users.noreply.github.com>
2021-07-09 01:45:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 $(function(){
   $('.cell').click(function(){
-    if ($(this).is('.red')){
-      $(this).toggleClass('white');
+    if ($(this).is('.white')){
+      $(this).removeClass('white').addClass('black');
     }
+    else
+    if ($(this).is('.black')){
+      $(this).removeClass('black').addClass('red');
+    }
+    else
+    if ($(this).is('.red')){
+      $(this).removeClass('red').addClass('orange');
+    }
+    else
+    if ($(this).is('.orange')){
+      $(this).removeClass('orange').addClass('yellow');
+    }
+    else
+    if ($(this).is('.yellow')){
+      $(this).removeClass('yellow').addClass('green');
+    }
+    else
+    if ($(this).is('.green')){
+      $(this).removeClass('green').addClass('blue');
+    }
+    else
+    if ($(this).is('.blue')){
+      $(this).removeClass('blue').addClass('purple');
+    }
+    else
+    if ($(this).is('.purple')){
+      $(this).removeClass('purple').addClass('white');
+    }
+  });
+  $('.reset').click(function(){
+    $('.cell').removeClass('purple blue green yellow orange red black').addClass('white');
   });
 });
