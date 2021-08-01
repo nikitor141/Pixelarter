76b650417abf64b471e3ad30b9a3c0f78623f313
nikitor141 <68957821+nikitor141@users.noreply.github.com>
2021-08-01 04:19:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,12 @@
 $(function(){
+  let color = ($('.color-picker').val());;
+  $('.color-picker').change(function(){
+    color = ($('.color-picker').val());
+  });
   $('.cell').click(function(){
-    if ($(this).is('.white')){
-      $(this).removeClass('white').addClass('black');
-    }
-    else
-    if ($(this).is('.black')){
-      $(this).removeClass('black').addClass('red');
-    }
-    else
-    if ($(this).is('.red')){
-      $(this).removeClass('red').addClass('orange');
-    }
-    else
-    if ($(this).is('.orange')){
-      $(this).removeClass('orange').addClass('yellow');
-    }
-    else
-    if ($(this).is('.yellow')){
-      $(this).removeClass('yellow').addClass('green');
-    }
-    else
-    if ($(this).is('.green')){
-      $(this).removeClass('green').addClass('blue');
-    }
-    else
-    if ($(this).is('.blue')){
-      $(this).removeClass('blue').addClass('purple');
-    }
-    else
-    if ($(this).is('.purple')){
-      $(this).removeClass('purple').addClass('white');
-    }
+    $(this).css('background-color', color);
   });
   $('.reset').click(function(){
-    $('.cell').removeClass('purple blue green yellow orange red black').addClass('white');
+    $('.cell').css('background-color', 'white');
   });
 });
