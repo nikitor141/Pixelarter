9d0a508655bc4786a62a9c6dd3a1afcdae576781
nikitor141 <nikitor141@gmail.com>
2023-07-25 01:09:30 +0300

    jquery

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
-$(function(){
-  let color = ($('.color-picker').val());;
-  $('.color-picker').change(function(){
-    color = ($('.color-picker').val());
-  });
-  $('.cell').click(function(){
-    $(this).css('background-color', color);
-  });
-  $('.reset').click(function(){
-    $('.cell').css('background-color', 'white');
+const colorPicker = document.querySelector('.color-picker');
+const field = document.querySelector('.field');
+const reset = document.querySelector('.reset');
+let color = colorPicker.value;
+
+//change color
+colorPicker.addEventListener('change', () => {
+  color = colorPicker.value;
+  console.log(color);
+});
+
+//draw
+// field.addEventListener('click', e => {
+//   if (e.target.closest('.cell')) {
+//     e.target.style.backgroundColor = color;
+//   };
+// });
+
+
+function fieldPressHandler(e) {
+  if (e.target.closest('.cell')) {
+    e.target.style.backgroundColor = color;
+  };
+};
+field.addEventListener('mousedown', (e) => {
+  if (e.target.closest('.cell')) {
+    e.target.style.backgroundColor = color;
+  }
+  field.addEventListener('mouseover', fieldPressHandler);
+});
+field.addEventListener('mouseup', () => {
+  field.removeEventListener('mouseover', fieldPressHandler);
+});
+field.addEventListener('mouseleave', () => {
+  field.removeEventListener('mouseover', fieldPressHandler);
+});
+
+//clear field
+reset.addEventListener('click', () => {
+  document.querySelectorAll('.cell[style]').forEach(cell => {
+    cell.removeAttribute('style');
   });
 });
